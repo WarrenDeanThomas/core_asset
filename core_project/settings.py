f7bb6c34b4ff0503cedca2c1fb5149cbb232a3af
warren <warrendeanthomas@gmail.com>
2022-05-20 13:12:20 +0400

    debug removed - set to false, secret key hidden

--- core_project/settings.py
+++ core_project/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ql06k_o((^#ot3oo)shh^x*^zy1+q=i31@lt1y1zdb6rtas)&@'
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.heroku.com']
 
 
 # Application definition
